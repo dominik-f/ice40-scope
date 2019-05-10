fb18507f2b9afd6a00d191153e8104c181ce246c
Dominik Fuchs <dominik.fuchs@outlook.at>
2019-05-10 21:14:15 +0200

    Add INotifyPropertyChanged

--- software/apps/ICE40Scope/MainViewModel.cs
+++ software/apps/ICE40Scope/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ using OxyPlot.Series;
 
 namespace ICE40Scope
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
-        public PlotModel MyModel { get; private set; }
+        private PlotModel mMyModel;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public PlotModel MyModel { get => mMyModel; private set { mMyModel = value; OnPropertyChanged(nameof(MyModel)); } }
+
 
         public MainViewModel(IMainView view)
         {
@@ -19,6 +24,11 @@ namespace ICE40Scope
 
             this.MyModel = new PlotModel { Title = "Example 1" };
             this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
